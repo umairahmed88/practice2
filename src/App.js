f14515379619543b650d9b6e770a66ad68939b77
umairahmed88 <umairahmed_88@ymail.com>
2020-06-01 14:35:31 +0500

    added dynamic contents and images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      samples: []
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.samples.map(sample => (
+          <h1 key={sample.id}>{sample.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
